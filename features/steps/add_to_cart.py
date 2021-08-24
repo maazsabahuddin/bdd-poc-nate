671b472f0ccfc4255e6b87c72f9855056d68f47e
faiq.khan <faiq.khan@techwards.co>
2021-08-24 13:32:27 +0500

    Skip all scenarios done

--- features/steps/add_to_cart.py
+++ features/steps/add_to_cart.py
@@ -8,11 +8,12 @@ def step_impl(context):
     add_to_cart.find_add_to_()
     context.current_obj = add_to_cart
 
-@when('add to cart found or check for overlays')
+@when('add to cart found or checking for overlays')
 def step_impl(context):
     if (not context.current_obj.check_cookies_overlay()):
         if (not context.current_obj.is_closeable_overlay()):
             if (context.current_obj.required_element is None):
+                context.current_obj.web.skip_all_remaining_scenarios()
                 context.scenario.skip(reason="Cannot find add to cart button.")
 
 @then('we should proceed to checkout')
