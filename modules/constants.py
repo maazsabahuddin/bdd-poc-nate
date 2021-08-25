f5d7d8910dd3148a8d7a2f907e5a7d4de1966a21
Maaz Sabah Uddin <maazsabahuddin@gmail.com>
2021-08-25 18:14:42 +0500

    create patterns

--- modules/constants.py
+++ modules/constants.py
@@ -8,7 +8,20 @@ class Tags:
 
 class Pattern:
     BUY_PATTERN = "//*[contains(text(),'Buy') or contains(text(),'BUY')]"
-    ACCEPT_COOKIES_PATTERN = "//*[(contains(text(),'Accept') and contains(text(),'Cookies')) or (contains(text(),'Accept') and contains(text(),'cookies')) or (contains(text(),'accept') and contains(text(),'cookies'))]"
+    ACCEPT_COOKIES_PATTERN = "//*[(contains(text(),'Accept') and contains(text(),'Cookies')) or " \
+                             "(contains(text(),'Accept') and contains(text(),'cookies')) or " \
+                             "(contains(text(),'accept') and contains(text(),'cookies'))]"
+    ADDRESS_PATTERN = "//*[contains(translate(text(), 'ADRES', 'adres'), 'adres')]"
+    FIRST_NAME = "x(//input[contains(@name, 'FirstName')])"
+    LAST_NAME = "x(//input[contains(@name, 'LastName')])"
+    EMAIL = "x(//input[contains(@name, 'Email')])"
+    PHONE = "x(//input[contains(@name, 'Phone')])"
+    ADDRESS1 = "x(//input[contains(@name, 'StreetLine1')])"
+    ADDRESS2 = "x(//input[contains(@name, 'StreetLine2')])"
+    CITY = "x(//input[contains(@name, 'AdministrativeArea')])"
+    STATE = "x(//input[contains(@name, 'Municipality')])"
+    POSTAL_CODE = "x(//input[contains(@name, 'PostalCode')])"
+    # COUNTRY = "x(//select[contains(@name, 'Country')])"
 
 
 class Scenarios:
