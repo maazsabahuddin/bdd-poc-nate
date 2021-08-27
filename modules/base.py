d122ac9a8534bb029eaba654f3d9f8e9cbaaf460
Maaz Sabah Uddin <maazsabahuddin@gmail.com>
2021-08-27 15:16:24 +0500

    update base.py file as same as sprint

--- modules/base.py
+++ modules/base.py
@@ -1,12 +1,11 @@
 # Framework imports
-from logging import exception
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.common import exceptions
 
 # Local
-from selenium.common import exceptions
-from modules.constants import SkipScenario
+from modules import constants
 
 
 class Base:
@@ -26,27 +25,26 @@ class Base:
         self.web_driver.quit()
     
     def skip_all_remaining_scenarios(self):
-        self.context._root[SkipScenario.SKIP_ALL] = True
+        self.context._root[constants.SkipScenario.SKIP_ALL] = True
     
     def skip_scenario(self, scenario_name):
-        self.context._root[SkipScenario.SKIP_SCENARIO].update({scenario_name: True})
+        self.context._root[constants.SkipScenario.SKIP_SCENARIO].update({scenario_name: True})
 
     def find_by_xpath_wait(self, xpath):
         return self.web_driver_wait.until(ec.visibility_of_element_located((By.XPATH, xpath)))
 
     def finds_by_xpath_wait(self, xpath):
-        return self.web_driver_wait.until(ec.presence_of_all_elements_located((By.XPATH, xpath)))
+        try:
+            elements = self.web_driver_wait.until(ec.presence_of_all_elements_located((By.XPATH, xpath)))
+            return elements
+        except exceptions.TimeoutException:
+            return []
 
     def finds_by_xpath_wait_until_element_clickable(self, xpath):
         return self.web_driver_wait.until(ec.element_to_be_clickable((By.XPATH, xpath))).click()
 
     def finds_by_xpath_and_set_attribute(self, xpath, value):
-        # return self.web_driver.find_element_by_xpath(xpath).setAttribute(constants.ETC.VALUE, value)
-        try:
-            elements= self.web_driver_wait.until(ec.presence_of_all_elements_located((By.XPATH, xpath)))
-            return elements
-        except exceptions.TimeoutException:
-            return []
+        return self.web_driver.find_element_by_xpath(xpath).setAttribute(constants.ETC.VALUE, value)
 
     def get_current_url(self):
         return self.web_driver.current_url
