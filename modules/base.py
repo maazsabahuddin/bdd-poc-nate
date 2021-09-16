2cc35eab221fe7289d4ed9d16063c57e29eafbab
faiq.khan <faiq.khan@techwards.co>
2021-09-16 15:42:24 +0500

    unused function removed.

--- modules/base.py
+++ modules/base.py
@@ -52,21 +52,6 @@ class Base:
 
     def finds_by_xpath(self, xpath):
         return self.web_driver.find_elements_by_xpath(xpath)
-    
-    def find_parent_element_from_child(self, child_element):
-        parent_elements_list = ["button", "a"]
-        try:
-            current_found_element = child_element
-            for x in range(2):
-                parent_element = current_found_element.find_element_by_xpath("..")
-                parent_element_name = parent_element.tag_name
-                if parent_element_name in parent_elements_list:
-                    return parent_element
-                else:
-                    current_found_element = parent_element
-            return None
-        except exceptions.NoSuchElementException:
-            return None
 
     def scroll_page(self, horizontal_axis, vertical_axis):
         query = f"window.scrollTo({horizontal_axis}, {vertical_axis})"
